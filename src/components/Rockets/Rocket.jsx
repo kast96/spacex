a5f0ef1e31b702df0f96f9b421e761b7949ae8b7
kast96 <kast96@yandex.ru>
2021-11-19 13:32:54 +0300

    formatting other properties

--- src/components/Rockets/Rocket.jsx
+++ src/components/Rockets/Rocket.jsx
@@ -33,9 +33,9 @@ const Rocket = ({rocket}) => {
 					<h3 className={s.rocket__props__title}>Общая информация</h3>
 					<RocketProperty title="Компания">{rocket.company}</RocketProperty>
 					<RocketProperty title="Страна">{rocket.country}</RocketProperty>
-					<RocketProperty title="Стоимость запуска">{rocket.cost_per_launch + '$'}</RocketProperty>
+					<RocketProperty title="Стоимость запуска">{rocket.cost_per_launch}$</RocketProperty>
 					<RocketProperty title="Дата первого запуска">{rocket.first_flight}</RocketProperty>
-					<RocketProperty title="Процент успеха">{rocket.success_rate_pct + '%'}</RocketProperty>
+					<RocketProperty title="Процент успеха">{rocket.success_rate_pct}%</RocketProperty>
 					<RocketProperty title="Тип">{rocket.type}</RocketProperty>
 					<RocketProperty title="Страница на wikipedia">
 						<a href="${rocket.wikipedia}" target="_blank">Открыть</a>
@@ -45,40 +45,60 @@ const Rocket = ({rocket}) => {
 					<h3 className={s.rocket__props__title}>Описание</h3>
 					<RocketProperty>{rocket.description}</RocketProperty>
 				</div>
-			</div>
-
-			<div>Количество ускорителей: {rocket.boosters}</div>
-			<div>Диаметр ракеты: {rocket.diameter.meters} м</div>
-			<div>Высора ракеты: {rocket.height.meters} м</div>
-			<div>Масса: {rocket.mass.kg} кг</div>
-			<div>Количество ступеней: {rocket.stages}</div>
-			<div>Первая ступень, время горения: {rocket.first_stage.burn_time_sec} сек</div>
-			<div>Первая ступень, кол-во двигателей: {rocket.first_stage.engines}</div>
-			<div>Первая ступень, кол-во топлива: {rocket.first_stage.fuel_amount_tons} тон</div>
-			<div>Первая ступень, сила над уровнем моря: {rocket.first_stage.thrust_sea_level.kN} кН</div>
-			<div>Первая ступень, сила в вакууме: {rocket.first_stage.thrust_vacuum.kN} кН</div>
-			<div>Вторая сутпень, время горения: {rocket.second_stage.burn_time_sec} сек</div>
-			<div>Вторая сутпень, кол-во двигателей: {rocket.second_stage.engines}</div>
-			<div>Вторая сутпень, кол-во топлива: {rocket.second_stage.fuel_amount_tons} тон</div>
-			<div>Количество посадочных ног: {rocket.landing_legs.number}</div>
-			<div>Материал посадочных ног: {rocket.landing_legs.material}</div>
-			<div>Полезная нагрузка: {rocket.payload_weights.map((payload_weight, index) => (
-				<div key={index}>
-					<div>Максимальная масса: {payload_weight.kg}</div>
-					<div>Название: {payload_weight.name}</div>
+				<div className={s.rocket__props__group}>
+					<h3 className={s.rocket__props__title}>Характеристики</h3>
+					<RocketProperty title="Диаметр ракеты">{rocket.diameter.meters} м</RocketProperty>
+					<RocketProperty title="Высора ракеты">{rocket.height.meters} м</RocketProperty>
+					<RocketProperty title="Масса">{rocket.mass.kg} кг</RocketProperty>
 				</div>
-			))}</div>
-			<div>Максимальная мощность двигателя: {rocket.engines.engine_loss_max}</div>
-			<div>Массовый импульс двигателя над уровнем моря: {rocket.engines.isp.sea_level}</div>
-			<div>Массовый импульс двигателя в вакууме: {rocket.engines.isp.vacuum}</div>
-			<div>Макет двигателя: {rocket.engines.layout}</div>
-			<div>Пропеллант 1: {rocket.engines.propellant_1}</div>
-			<div>Пропеллант 2: {rocket.engines.propellant_2}</div>
-			<div>Cила двигателя над уровнем моря: {rocket.engines.thrust_sea_level.kN} кН</div>
-			<div>Cила двигателя в вакууме: {rocket.engines.thrust_vacuum.kN} кН</div>
-			<div>Тяга к весу двигателя: {rocket.engines.thrust_to_weight}</div>
-			<div>Тип двигателя: {rocket.engines.type}</div>
-			<div>Версия двигателя: {rocket.engines.version}</div>
+				<div className={s.rocket__props__group}>
+					<h3 className={s.rocket__props__title}>Характеристики</h3>
+					<RocketProperty title="Диаметр ракеты">{rocket.diameter.meters} м</RocketProperty>
+					<RocketProperty title="Высора ракеты">{rocket.height.meters} м</RocketProperty>
+					<RocketProperty title="Масса">{rocket.mass.kg} кг</RocketProperty>
+					<RocketProperty title="Количество ускорителей">{rocket.boosters}</RocketProperty>
+					<RocketProperty title="Количество ступеней">{rocket.stages}</RocketProperty>
+					<RocketProperty title="Количество посадочных ног">{rocket.landing_legs.number}</RocketProperty>
+					<RocketProperty title="Материал посадочных ног">{rocket.landing_legs.material}</RocketProperty>
+				</div>
+				<div className={s.rocket__props__group}>
+					<h3 className={s.rocket__props__title}>Двигатель</h3>
+					<RocketProperty title="Тип двигателя">{rocket.engines.type}</RocketProperty>
+					<RocketProperty title="Версия двигателя">{rocket.engines.version}</RocketProperty>
+					<RocketProperty title="Макет двигателя">{rocket.engines.layout}</RocketProperty>
+					<RocketProperty title="Максимальная мощность двигателя">{rocket.engines.engine_loss_max}</RocketProperty>
+					<RocketProperty title="Тяга к весу двигателя">{rocket.engines.thrust_to_weight}</RocketProperty>
+					<RocketProperty title="Массовый импульс двигателя над уровнем моря">{rocket.engines.isp.sea_level}</RocketProperty>
+					<RocketProperty title="Массовый импульс двигателя в вакууме">{rocket.engines.isp.vacuum}</RocketProperty>
+					<RocketProperty title="Пропеллант 1">{rocket.engines.propellant_1}</RocketProperty>
+					<RocketProperty title="Пропеллант 2">{rocket.engines.propellant_2}</RocketProperty>
+					<RocketProperty title="Cила двигателя над уровнем моря">{rocket.engines.thrust_sea_level.kN} кН</RocketProperty>
+					<RocketProperty title="Cила двигателя в вакууме">{rocket.engines.thrust_vacuum.kN} кН</RocketProperty>
+				</div>
+				<div className={s.rocket__props__group}>
+					<h3 className={s.rocket__props__title}>Первая ступень</h3>
+					<RocketProperty title="Время горения">{rocket.first_stage.burn_time_sec} сек</RocketProperty>
+					<RocketProperty title="Количество двигателей">{rocket.first_stage.engines}</RocketProperty>
+					<RocketProperty title="Количество топлива">{rocket.first_stage.fuel_amount_tons} тон</RocketProperty>
+					<RocketProperty title="Сила над уровнем моря">{rocket.first_stage.thrust_sea_level.kN} кН</RocketProperty>
+					<RocketProperty title="Сила в вакууме">{rocket.first_stage.thrust_vacuum.kN} кН</RocketProperty>
+				</div>
+				<div className={s.rocket__props__group}>
+					<h3 className={s.rocket__props__title}>Вторая ступень</h3>
+					<RocketProperty title="Время горения">{rocket.second_stage.burn_time_sec} сек</RocketProperty>
+					<RocketProperty title="Количество двигателей">{rocket.second_stage.engines}</RocketProperty>
+					<RocketProperty title="Количество топлива">{rocket.second_stage.fuel_amount_tons} тон</RocketProperty>
+				</div>
+				<div className={s.rocket__props__group}>
+					<h3 className={s.rocket__props__title}>Полезная нагрузка</h3>
+					{rocket.payload_weights.map((payload_weight, index) => (
+						<div key={index}>
+							<RocketProperty title="Название">{payload_weight.name} кг</RocketProperty>
+							<RocketProperty title="Максимальная масса">{payload_weight.kg} кг</RocketProperty>
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 }
